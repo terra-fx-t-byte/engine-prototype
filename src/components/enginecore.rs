b96ab0353d4fdfb3ee462933c82286d3235ea888
terra-fx-t-byte <228866656+terra-fx-t-byte@users.noreply.github.com>
2025-12-24 00:12:08 +0500

    Add files via upload

--- src/components/enginecore.rs
+++ src/components/enginecore.rs
@@ -4,11 +4,11 @@ use std::collections::HashMap;
 use std::any::{Any, TypeId};
 
 #[allow(unused)]
-type Entity = u32;
+pub type Entity = u32;
 
 #[allow(unused)]
 #[allow(nonstandard_style)]
-struct Universe{
+pub struct Universe{
     entities: Vec<Entity>,
     components: HashMap<TypeId, HashMap<Entity, Box<dyn Any>>>,
     next_EID: Entity,
@@ -17,33 +17,33 @@ struct Universe{
 #[allow(unused)]
 #[allow(nonstandard_style)]
 impl Universe {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self { entities: Vec::new(), components: HashMap::new(), next_EID: 0 }
     }
-    fn create_enitiy(&mut self) -> Entity {
+    pub fn create_enitiy(&mut self) -> Entity {
         let ent: u32 = self.next_EID;
         self.next_EID += 1;
         self.entities.push(ent);
         ent
     }
-    fn add_component<T: 'static>(&mut self, _entity: Entity, _component: T) {
+    pub fn add_component<T: 'static>(&mut self, _entity: Entity, _component: T) {
         let type_id: TypeId = TypeId::of::<T>();
         let component_map: &mut HashMap<u32, Box<dyn Any + 'static>> = self.components.entry(type_id).or_insert_with(HashMap::new);
         component_map.insert(_entity, Box::new(_component));
     }
-    fn get_component<T: 'static>(&self, _entity: Entity) -> Option<&T> {
+    pub fn get_component<T: 'static>(&self, _entity: Entity) -> Option<&T> {
         let type_id: TypeId = TypeId::of::<T>();
         self.components.get(&type_id)?
             .get(&_entity)?
             .downcast_ref::<T>()
     }
-    fn get_component_mut<T: 'static>(&mut self, _entity: Entity) -> Option<&mut T> {
+    pub fn get_component_mut<T: 'static>(&mut self, _entity: Entity) -> Option<&mut T> {
         let type_id: TypeId = TypeId::of::<T>();
         self.components.get_mut(&type_id)?
             .get_mut(&_entity)?
             .downcast_mut::<T>()
     }
-    fn query<T: 'static>(&self) -> impl Iterator <Item = (Entity, &T)> {
+    pub fn query<T: 'static>(&self) -> impl Iterator <Item = (Entity, &T)> {
         let type_id = TypeId::of::<T>();
         self.components
             .get(&type_id)
--- src/components/readycomponents.rs
+++ src/components/readycomponents.rs
@@ -11,45 +11,17 @@ pub enum Engine_enum_shape_3D {
 
 #[allow(unused)]
 #[allow(nonstandard_style)]
-pub struct Engine_object_3D {
+pub struct Engine_body_3D {
     position: raylib::ffi::Vector3,
     rendermodel: Option<raylib::ffi::Model>,
+    geometryshape: Option<Engine_enum_shape_3D>,
     color: raylib::ffi::Color,
 }
 
 #[allow(unused)]
 #[allow(nonstandard_style)]
-impl Engine_object_3D {
-    pub fn new(position: raylib::ffi::Vector3, c: raylib::ffi::Color, shape: Option<Engine_enum_shape_3D>) -> Self{
-        let res: Option<raylib::ffi::Model>;
-        match shape {
-            Some(Data) => {
-                match Data {
-                    Engine_enum_shape_3D::AS_BALL { r } => {
-                        unsafe {
-                            res = Some(raylib::ffi::LoadModelFromMesh(raylib::ffi::GenMeshSphere(r, 16, 16)));
-                        }
-                    },
-                    Engine_enum_shape_3D::AS_RECTANGLE { x, y, z } => {
-                        unsafe {
-                            res = Some(raylib::ffi::LoadModelFromMesh(raylib::ffi::GenMeshCube(x, y, z)));
-                        }
-                    }
-                }
-            },
-            None => {
-                res = None;
-            },
-        }
-        Self { position, rendermodel: res, color: c }
-    }
-    pub fn draw(&mut self) {
-        if let Some(Data) = self.rendermodel {
-            unsafe {
-                raylib::ffi::DrawModel(Data, self.position, 1.0, self.color);
-            }
-        }
-    }
+impl Engine_body_3D {
+    
 }
 
 // that's for using new function in FFI instead of prelude, more convinient for imo
@@ -68,3 +40,90 @@ impl Easypaint for raylib::ffi::Color {
     }
 }
 
+#[allow(unused)]
+#[allow(nonstandard_style)]
+#[derive(Debug, Clone, Copy)]
+pub struct Parent(Option<engine_core::Entity>);
+
+#[allow(unused)]
+#[allow(nonstandard_style)]
+pub struct Children(Option<Vec<engine_core::Entity>>);
+
+
+// Gotta rework it somehow so i dont need to ask for current entity in functions but still get current entity
+#[allow(unused)]
+#[allow(nonstandard_style)]
+impl Parent {
+    pub fn setParent(&mut self, currentEntity: engine_core::Entity, np:engine_core::Entity, univ: &mut engine_core::Universe){
+        if let Some(p) = self.0 {
+            let old_parent: engine_core::Entity = p;
+            if old_parent == np {
+                return;
+            }
+            if let Some(oldChildren) = univ.get_component_mut::<Children>(old_parent) {
+                Children::removeChild(oldChildren, currentEntity);
+            }
+            self.0 = Some(np);
+            if let Some(newChildren) = univ.get_component_mut::<Children>(np) {
+                newChildren.addChild(currentEntity);
+            } else {
+                let mut nchildren: Children = Children(None);
+                nchildren.addChild(currentEntity);
+            }
+        }
+    }
+    pub fn getParent(&self) -> Option<engine_core::Entity> {
+        self.0
+    }
+    pub fn getParent_ref(&self) -> Option<&engine_core::Entity> {
+        self.0.as_ref()
+    }
+    pub fn getParent_mut_ref(&mut self) -> Option<&mut engine_core::Entity> {
+        self.0.as_mut()
+    }
+}
+
+#[allow(unused)]
+#[allow(nonstandard_style)]
+pub trait GetCurrentEntity {
+    fn GetContextEntity(&self) -> engine_core::Entity;
+}
+
+#[allow(unused)]
+#[allow(nonstandard_style)]
+pub struct EntityContext {
+    currentEntity: engine_core::Entity,
+}
+
+impl GetCurrentEntity for EntityContext {
+    fn GetContextEntity(&self) -> engine_core::Entity {
+        self.currentEntity
+    }
+}
+
+#[allow(unused)]
+#[allow(nonstandard_style)]
+impl Children {
+    pub fn hasChildren(&self) -> bool{
+        if let Some(c) = &self.0 {
+            if c.len() > 0 {return true};
+        }
+        false
+    }
+    pub fn addChild(&mut self, child: engine_core::Entity) {
+        if let Some(c) = &mut self.0 {
+            c.push(child);
+        } else {
+            // handling problem where vector does not exists
+            self.0 = Some(Vec::new());
+            if let Some(handleNoChild) = &mut self.0 {
+                handleNoChild.push(child);
+            }
+        }
+    }
+    pub fn removeChild(&mut self, child: engine_core::Entity) {
+        if let Some(vec_children) = &mut self.0 {
+            vec_children.retain(|&v_child| v_child != child);
+        }
+    }
+}
